6f4593a12f64363a7c510d6c13953baaa3ca74fb
Jim Veneskey <github@strifecrafter.com>
2015-05-07 13:00:00 -0400

    Drew the Arc UI above the buttons.  Left off here

---
+++ Flo/CounterView.swift
@@ -0,0 +1,50 @@
+//
+//  CounterView.swift
+//  Flo
+//
+//  Created by jim Veneskey on 5/7/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+let noOfGlasses = 8
+let π:CGFloat = CGFloat(M_PI)
+
+
+@IBDesignable class CounterView: UIView {
+    @IBInspectable var counter: Int = 5
+    @IBInspectable var outlineColor: UIColor = UIColor.blueColor()
+    @IBInspectable var counterColor: UIColor = UIColor.orangeColor()
+    
+
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+        // 1
+        let center = CGPoint(x:bounds.width/2, y:bounds.height/2)
+        
+        //2
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        
+        //3
+        let arcWidth: CGFloat = 76
+        
+        //4
+        let startAngle: CGFloat = 3 * π / 4
+        let endAngle : CGFloat = π / 4
+        
+        //5
+        var path = UIBezierPath(arcCenter: center,
+            radius: radius/2 - arcWidth/2,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: true)
+        
+        //6
+        path.lineWidth = arcWidth
+        counterColor.setStroke()
+        path.stroke()
+        
+    }
+
+}
